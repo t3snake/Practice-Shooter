50918ca3770c24a907ced5a05344068d69bc2b7d
Tarun Menon <t3snake@gmail.com>
2020-12-01 12:34:58 +0530

    Project Files

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+
+    public float spinSpeed = 180.0f;
+    public float motionSpeed = 0.05f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        float y = Mathf.Cos(Time.timeSinceLevelLoad) * motionSpeed;
+        float z = 3 * Mathf.Sin(Time.timeSinceLevelLoad) * motionSpeed;
+        transform.Translate(Vector3.up * y + Vector3.right * z);
+        Debug.Log(Vector3.up);
+        Debug.Log(z);
+        Debug.Log(Vector3.up * z);
+    }
+}
---
+++ Assets/Scripts/HitTextTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HitTextTimer : MonoBehaviour
+{
+	public static HitTextTimer htt;
+
+	public float timeOut = 1.0f;
+	private Text hitText;
+
+	// Use this for initialization
+	void Start()
+	{
+		// invote the DestroyNow funtion to run after timeOut seconds
+		htt = gameObject.GetComponent<HitTextTimer>();
+		hitText = gameObject.GetComponent<Text>();
+		hitText.text = "";
+	}
+
+	void EmptyNow()
+	{
+		// destory the game Object
+		hitText.text = "";
+	}
+
+	public void SetText(string text)
+	{
+		hitText.text = text;
+		Invoke("EmptyNow", timeOut);
+	}
+}
